9dc380114475f0cc02f05bfb9ce6d7ddc4aff3f2
BrianMarquez3 <brian3marquez@gmail.com>
2023-07-11 06:39:16 -0500

    post

--- APICore/Controllers/ProductoController.cs
+++ APICore/Controllers/ProductoController.cs
@@ -57,5 +57,80 @@ namespace APICore.Controllers
             }
 
         }
+        [HttpGet]
+        [Route("Obtener/{idProducto:int}")]
+        public IActionResult Obtener(int idProducto)
+        {
+            List<Producto> lista = new List<Producto>();
+            Producto producto = new Producto();
+
+
+            try
+            {
+                using (var conexion = new SqlConnection(CadenaSQL))
+                {
+
+                    conexion.Open();
+                    var cmd = new SqlCommand("sp_lista_productos", conexion);
+                    cmd.CommandType = CommandType.StoredProcedure;
+
+                    using (var rd = cmd.ExecuteReader())
+                    {
+
+                        while (rd.Read())
+                        {
+                            lista.Add(new Producto()
+                            {
+
+                                IdProducto = Convert.ToInt32(rd["IdProducto"]),
+                                CodigoBarra = rd["CodigoBarra"].ToString(),
+                                Nombre = rd["Nombre"].ToString(),
+                                Marca = rd["Marca"].ToString(),
+                                Categoria = rd["Categoria"].ToString(),
+                                Precio = Convert.ToDecimal(rd["Precio"])
+                            });
+                        }
+                    }
+                }
+                producto = lista.Where(item => item.IdProducto == idProducto).FirstOrDefault();
+
+
+                return StatusCode(StatusCodes.Status200OK, new { mensaje = "ok", response = producto });
+            }
+            catch (Exception error)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, new { mensaje = error.Message, response = producto });
+            }
+
+        }
+        [HttpPost]
+        [Route("Guardar")]
+        public IActionResult Guardar([FromBody] Producto objeto)
+        {
+            try
+            {
+
+                using (var conexion = new SqlConnection(CadenaSQL))
+                {
+                    conexion.Open();
+                    var cmd = new SqlCommand("sp_guardar_producto", conexion);
+                    cmd.Parameters.AddWithValue("codigoBarra", objeto.CodigoBarra);
+                    cmd.Parameters.AddWithValue("nombre", objeto.Nombre);
+                    cmd.Parameters.AddWithValue("marca", objeto.Marca);
+                    cmd.Parameters.AddWithValue("categoria", objeto.Categoria);
+                    cmd.Parameters.AddWithValue("precio", objeto.Precio);
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.ExecuteNonQuery();
+                }
+
+                return StatusCode(StatusCodes.Status200OK, new { mensaje = "agregado" });
+            }
+            catch (Exception error)
+            {
+
+                return StatusCode(StatusCodes.Status500InternalServerError, new { mensaje = error.Message });
+
+            }
+        }
     }
 }
--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-# API-CORE
+## API-CORE
 
+
+## GET
+
+http://localhost:5265/api/Producto/Lista
+http://localhost:5265/api/Producto/Obtener/1
+## POST
+
+{
+  "idProducto": 0,
+  "codigoBarra": "0101",
+  "nombre": "demo",
+  "marca": "demo",
+  "categoria": "demo",
+  "precio": 10
+}
+ 
