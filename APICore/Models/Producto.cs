87bcd383c6a9641d45837f6038ae5097c373a626
BrianMarquez3 <brian3marquez@gmail.com>
2023-07-10 21:51:25 -0500

    Models

---
+++ APICore/Models/Producto.cs
@@ -0,0 +1,17 @@
+﻿namespace APICore.Models
+{
+    public class Producto
+    {
+        public int IdProducto { get; set; }
+        public int CodigoBarra { get; set; }
+
+        public int Nombre { get; set; }
+
+        public int Marca { get; set; }
+
+        public int Categoria { get; set; }
+
+        public int Precio { get; set; }
+
+    }
+}
